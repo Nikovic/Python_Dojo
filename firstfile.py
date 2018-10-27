fb15a3454f595684978773550fa064f44d08d238
NikiH <NikiHerl@gmail.com>
2018-10-27 20:56:38 +0200

    add sample comments

--- firstfile.py
+++ firstfile.py
@@ -1,1 +1,5 @@
+# this is a comment
+# default shortcut zum commenten/uncommenten ist <Ctrl + "/">
+# (was auf deutschen Tastaturen nicht gut geht --> das "/" am Numpad funzt)
+
 print("hallo niki")
