6ef3697dbbbc8b1ada7d5b10e3e5e51ebb9ab779
NikiH <NikiHerl@gmail.com>
2018-11-03 12:17:00 +0100

    Update firstfile.py
    add further details on comments

--- firstfile.py
+++ firstfile.py
@@ -2,4 +2,10 @@
 # default shortcut zum commenten/uncommenten ist <Ctrl + "/">
 # (was auf deutschen Tastaturen nicht gut geht --> das "/" am Numpad funzt)
 
+''' this kind of multi-line comment
+should actually not be used (for normal comment-y type stuff) '''
+
+""" use double quotes
+instead """
+
 print("hallo niki")
