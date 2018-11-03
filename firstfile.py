41425be3999256c29734463d29094ccfc4bfca0b
NikiH <NikiHerl@gmail.com>
2018-11-03 16:08:11 +0100

    Update firstfile.py
    did some experiments with text I/O and string-to-bool casting

--- firstfile.py
+++ firstfile.py
@@ -8,4 +8,13 @@ should actually not be used (for normal comment-y type stuff) '''
 """ use double quotes
 instead """
 
-print("hallo niki")
+print("hallo")
+
+# seems like input() reads the typed-in stuff as a String
+boolean = input("type something that will be evaluated:")
+
+# and also seems like (non-empty) strings evaluate to True
+if (boolean):
+    print("input eval'd to True")
+
+inlineIf = "condition true" if boolean == "False" else "condition false"
